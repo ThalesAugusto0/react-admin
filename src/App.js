095dba6e392daf7d8a67a684e8b0f80f0c891322
Thales Augusto <thalllesaugustos28@gmail.com>
2022-11-23 23:32:31 -0300

    Adicionado topbar e sidebar alem dos botoes da topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+
 function App() {
-  return <div className="app"></div>
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={Dashboard} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
 
 export default App;
